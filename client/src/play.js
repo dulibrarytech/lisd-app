32605c52ba5ea8f13ff18bafecb947d228285290
Jeff <you@example.com>
2016-07-24 12:35:10 -0600

    Add play view files, add mysql package to server package.json

---
+++ client/src/play.js
@@ -0,0 +1,12 @@
+export class Play {
+  heading = 'Play view:';
+
+  simulate() {
+
+  }
+
+  commit() {
+
+  }
+
+}
--- client/src/welcome.js
+++ client/src/welcome.js
@@ -1,7 +1,17 @@
+import {Router} from 'aurelia-router';
+
 export class Welcome {
+
+  static inject() { return [Router]; }
+
+  constructor(router){
+    this.theRouter = router;
+  }
+
   heading = 'Welcome to the Data Warehouse App!';
   
   start() {
+    this.theRouter.navigate("users");
     console.log("start");
   }
 }
