e6e696c2dbd1edde51d257c60cdd436b15544f94
Jeff <you@example.com>
2016-07-24 12:53:55 -0600

    Add notes

--- client/src/play.js
+++ client/src/play.js
@@ -1,6 +1,20 @@
 export class Play {
   heading = 'Play view:';
 
+  // render();
+
+  render() {
+  	// load()
+
+  	// set all view vars
+  }
+
+  load() {
+  	// request current data for active user
+
+  	// cache data
+  }
+
   simulate() {
 
   }
