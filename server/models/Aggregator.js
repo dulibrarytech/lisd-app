3373d34d0fbc5f65adab2f52872030672cd37aa7
Jeff <you@example.com>
2016-08-22 15:12:40 -0600

    test getAllStudents(), async

--- server/models/Aggregator.js
+++ server/models/Aggregator.js
@@ -4,16 +4,20 @@ require('dotenv').config();
 var settings = require("../config/settings.js");
 var database = require('../lib/Database.js');
 
+
+
 /**
 *
 *
 */
 exports.getTotalStudents = function(fromYear, toYear, listByMonth, librarianID) {
 	
-	var resultSet = {};
+	var resultSet = {totalStudents: null};
 
 	var fromDate = fromYear + '-' + settings.fiscalYearStart;
 	var toDate = toYear + '-' + settings.fiscalYearEnd;
+
+	// Undergraduates
 	var query = 'SELECT SUM(undergraduates) FROM tbl_lisd WHERE classDate >= "' + fromDate + '" AND classDate <= "' + toDate + '"';
 	database.query(query, function (error, results, fields) {
 	  
@@ -21,12 +25,15 @@ exports.getTotalStudents = function(fromYear, toYear, listByMonth, librarianID) 
 		console.log("Database error: " + error);
 	  }
 	  else {
-		if(results.length > 0) {
-			// Gather student totals, build result set
-			console.log(results);
+	  	var totalStudents = results[0]['SUM(undergraduates)'];
+		if(results.length > 0 && typeof totalStudents == "number") {
+			resultSet.totalStudents = totalStudents;
 		}
 	  }
+	  console.log("1");
 	});
+
+	console.log("2");
 
 	return resultSet;
 }
