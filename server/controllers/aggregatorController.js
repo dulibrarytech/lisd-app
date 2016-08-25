f236a793179372a2f2e4182047983d056f218be3
Jeff <you@example.com>
2016-08-25 11:17:21 -0600

    Add controllers

---
+++ server/controllers/aggregatorController.js
@@ -0,0 +1,10 @@
+'use strict'
+
+require('dotenv').config();
+var settings = require("../config/settings.js");
+var aggregatorModel = require("../models/Aggregator.js");
+
+exports.getAllData = function() {
+
+	console.log("AC");
+};
