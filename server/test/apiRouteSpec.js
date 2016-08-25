383c30b6e54663e7598d4605712ba459743e7f01
Jeff <you@example.com>
2016-08-25 16:00:49 -0600

    GC

--- server/test/apiRouteSpec.js
+++ server/test/apiRouteSpec.js
@@ -16,4 +16,15 @@ describe("Server API", function(){
 		    });
 		});
 	});
+
+	// describe("#GET /get/data/librarian/:id", function(){
+	// 	it('should return status 200', function(done) {
+	// 	  chai.request(process.env.BASE_URL)
+	// 	    .get('/get/data/librarian/1')
+	// 	    .end(function(err, res){
+	// 	      expect(res).to.have.status(200);
+	// 	      done();
+	// 	    });
+	// 	});
+	// });
 });
