7086e92b50be167da4644785cf8457378d0357ee
Jeff <you@example.com>
2016-08-26 10:15:52 -0600

    Pre model refactor

--- server/controllers/aggregatorController.js
+++ server/controllers/aggregatorController.js
@@ -5,38 +5,50 @@ var settings = require("../config/settings.js");
 var aggregatorModel = require("../models/Aggregator.js")
 
 
+
+// exports.getData = function(req, res) {
+
+// }
+
+
+
 exports.getAllData = function(req, res) {
 
 	var allResults = [];
 	var maxLength = Object.keys(aggregatorModel).length;
 	var response = res;
+	var fromYear = '2015', toYear = '2016';
 
-	// TODO get data from req
+	// TODO get data from req.  Set data, pass whole date into model.
+	// var fromDate = fromYear + '-' + settings.fiscalYearStart; // ***TODO 	MOVE THESE UP TO THE CONTROLLER!!  REMOVE DOTENV INCLUDES!
+	// var toDate = toYear + '-' + settings.fiscalYearEnd;
 	var data = {
-		fromYear: '2015',
-		toYear: '2016',
+		fromDate: fromYear + '-' + settings.fiscalYearStart,
+		toDate: toYear + '-' + settings.fiscalYearEnd,
 		listByMonth: 0,
 		librarianID: 0
 	};
 
+	var appendDataToResponseObject = function(data) {
+		// Loop keys of data object, adding each key and its data to the main results object
+		for(var key in data) {
+
+		}
+	}
+
 	var sendResponse = function(results) {
 		allResults.push(results);	// TODO add under separate keys for each function return
+		//appendDataToResponseObject(results);
 		if(allResults.length >= maxLength) {
 			response.send(allResults);
 			//done();
 		}
 	};
 
-	// aggregatorModel.getTotalStudents(data, sendResponse);
-	// aggregatorModel.getDepartmentTotals(data, sendResponse);
-
+	// Aggregate data from DB model (Async)
 	for(var key in aggregatorModel) {
 		aggregatorModel[key](data, sendResponse);
 	}
-
-	//console.log(aggregatorModel);
-
-	//res.send({test:12345});
 };
 
 exports.getStatsByMonth = function() {
