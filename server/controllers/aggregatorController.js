1320a57df73e1066ad4cf09a47ef8397422d5ac8
Jeff <you@example.com>
2016-08-28 13:42:28 -0600

    Re-sort functions

--- server/controllers/aggregatorController.js
+++ server/controllers/aggregatorController.js
@@ -4,24 +4,13 @@ require('dotenv').config();
 var settings = require("../config/settings.js");
 var aggregatorModel = require("../models/Aggregator.js")
 
-
-
-// exports.getData = function(req, res) {
-
-// }
-
-
-
-exports.getAllData = function(req, res) {
+exports.getDataAll = function(req, res) {
 
 	var allResults = [];
 	var maxLength = Object.keys(aggregatorModel).length;
 	var response = res;
 	var fromYear = '2015', toYear = '2016';
 
-	// TODO get data from req.  Set data, pass whole date into model.
-	// var fromDate = fromYear + '-' + settings.fiscalYearStart; // ***TODO 	MOVE THESE UP TO THE CONTROLLER!!  REMOVE DOTENV INCLUDES!
-	// var toDate = toYear + '-' + settings.fiscalYearEnd;
 	var data = {
 		fromDate: fromYear + '-' + settings.fiscalYearStart,
 		toDate: toYear + '-' + settings.fiscalYearEnd,
@@ -29,28 +18,15 @@ exports.getAllData = function(req, res) {
 		librarianID: 0
 	};
 
-	var appendDataToResponseObject = function(data) {
-		// Loop keys of data object, adding each key and its data to the main results object
-		for(var key in data) {
-
-		}
-	}
-
 	var sendResponse = function(results) {
-		allResults.push(results);	// TODO add under separate keys for each function return
-		//appendDataToResponseObject(results);
-		if(allResults.length >= maxLength) {
-			response.send(allResults);
-			//done();
-		}
+		response.send(allResults);
+		//done();
+
 	};
 
-	// Aggregate data from DB model (Async)
-	for(var key in aggregatorModel) {
-		aggregatorModel[key](data, sendResponse);
-	}
+	//aggregatorModel.getAllData(data, sendResponse);
 };
 
-exports.getStatsByMonth = function() {
+exports.getDataByLibrarian = function() {
 
 };
--- server/models/Aggregator.js
+++ server/models/Aggregator.js
@@ -4,49 +4,62 @@ require('dotenv').config();
 var settings = require("../config/settings.js");
 var database = require('../lib/Database.js');
 
+// *** This will get data for both sections of the first page.
+// data:  If librarianID is present, get data for that librarian.  If byMonth is true, will aggregate results by monts.
+exports.getAllData = function(data, callback, done) {
+	
+	// Aggregate data from DB model (Async)
+	// for(var key  in aggregatorModel) {
+	// 	//aggregatorModel[key](data, sendResponse);
+	// 	console.log(key);
+	// }
 
+	var appendDataToResponseObject = function(data) {
+		// Loop keys of data object, adding each key and its data to the main results object
+		for(var key in data) {
 
-// exports.getAllData = function(data, callback, done) {
+		}
+	}
 
-// }
+	// var sendResponse = function(results) {
+	// 	allResults.push(results);	// TODO add under separate keys for each function return
+	// 	//appendDataToResponseObject(results);
+	// 	if(allResults.length >= maxLength) {
+	// 		response.send(allResults);
+	// 		//done();
+	// 	}
+	// };
 
+	var getTotalStudents = function(data, callback, done) {
 
+		var resultObj = {};
 
-/**
-* TODO list data object members
-* @param object data 	data object members:
-*
-* 	object callback		Function is called once the async data retrieval is complete.  resultSet object is passed into this function, for external processing.
-* 	function done 		For testing purposes only (mocha-chai asynchronous testing)
-*/
-exports.getTotalStudents = function(data, callback, done) {
+		var query = 'SELECT SUM(undergraduates) AS underGraduates, SUM(graduates) AS graduates, SUM(faculty) AS faculty, SUM(other) AS other FROM tbl_lisd WHERE classDate >= "' + data.fromDate + '" AND classDate <= "' + data.toDate + '"';
+		database.query(query, function (error, results, fields) {
+		  
+		  if(error) {
+			console.log("Database error: " + error);
+		  }
+		  // TODO utilize resultObj
+		  callback(results,done); // TODO test results for db error?  ex. what is returned with a db error, is it an array?  does it have empty object?  Test and move to getDepartmentTotals()
+		});
+	};
 
-	var resultObj = {};
+	var getTotalStudentsByDepartment = function(data, callback, done) {
+		callback({deptTotals: '18'},done);
+	};
 
-	var query = 'SELECT SUM(undergraduates) AS underGraduates, SUM(graduates) AS graduates, SUM(faculty) AS faculty, SUM(other) AS other FROM tbl_lisd WHERE classDate >= "' + data.fromDate + '" AND classDate <= "' + data.toDate + '"';
-	database.query(query, function (error, results, fields) {
-	  
-	  if(error) {
-		console.log("Database error: " + error);
-	  }
-	  // TODO utilize resultObj
-	  callback(results,done); // TODO test results for db error?  ex. what is returned with a db error, is it an array?  does it have empty object?  Test and move to getDepartmentTotals()
-	});
-};
+	// exports.getTotalStudentsByDepartment = function(data, callback, done) {
 
-exports.getTotalStudentsByDepartment = function(data, callback, done) {
-	callback({deptTotals: '18'},done);
-};
+	// };
+	// exports.getTotalClassesByDepartment = function(data, callback, done) {
 
-// exports.getTotalStudentsByDepartment = function(data, callback, done) {
+	// };
+	// exports.getTotalClassesByLocation = function(data, callback, done) {
 
-// };
-// exports.getTotalClassesByDepartment = function(data, callback, done) {
+	// };
+	// exports.getTotalClassesByClassType = function(data, callback, done) {
 
-// };
-// exports.getTotalClassesByLocation = function(data, callback, done) {
+	// };
+}
 
-// };
-// exports.getTotalClassesByClassType = function(data, callback, done) {
-
-// };
