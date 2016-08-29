eddb825d08efa1b2519eac54b3be71f925353a82
Jeff <you@example.com>
2016-08-29 11:43:34 -0600

    Add model object

--- server/models/Aggregator.js
+++ server/models/Aggregator.js
@@ -9,60 +9,60 @@ var database = require('../lib/Database.js');
 exports.getAllData = function(data, callback, done) {
 	
 	var allResults = [];
-	var maxLength = Object.keys(aggregatorModel).length;
+	var count = Object.keys(model).length;
+	console.log(count);
+
+	// Aggregate data from DB model functions (Async)
+	// for(var key in model) {
+	// 	model[key](data, callback, done);
+	// }
+
+	var appendDataToResponseObject = function(data) {
+		// Loop keys of data object, adding each key and its data to the main results object
+		for(var key in data) {
+
+		}
+
+		if(allResults.length >= count) {
+			//callback(allresults,done);
+		}
+	};
+
+	model.getTotalStudents(data, callback, done);
 }
 
-// Aggregate data from DB model (Async)
-// for(var key  in aggregatorModel) {
-// 	//aggregatorModel[key](data, sendResponse);
-// 	console.log(key);
-// }
+var model = {
 
-var appendDataToResponseObject = function(data) {
-	// Loop keys of data object, adding each key and its data to the main results object
-	for(var key in data) {
+	getTotalStudents : function(data, callback, done) {
 
+		var resultObj = {};
+
+		var query = 'SELECT SUM(undergraduates) AS underGraduates, SUM(graduates) AS graduates, SUM(faculty) AS faculty, SUM(other) AS other FROM tbl_lisd WHERE classDate >= "' + data.fromDate + '" AND classDate <= "' + data.toDate + '"';
+		database.query(query, function (error, results, fields) {
+		  
+		  if(error) {
+			console.log("Database error: " + error);
+		  }
+		  // TODO utilize resultObj
+		  callback(results,done); // TODO test results for db error?  ex. what is returned with a db error, is it an array?  does it have empty object?  Test and move to getDepartmentTotals()
+		});
+	},
+
+	getTotalStudentsByDepartment : function(data, callback, done) {
+		callback([{deptTotals: '18'}],done);
 	}
+
+	// var getTotalStudentsByDepartment = function(data, callback, done) {
+
+	// };
+	// var getTotalClassesByDepartment = function(data, callback, done) {
+
+	// };
+	// var getTotalClassesByLocation = function(data, callback, done) {
+
+	// };
+	// var getTotalClassesByClassType = function(data, callback, done) {
+
+	// };
 }
-
-// var sendResponse = function(results) {
-// 	allResults.push(results);	// TODO add under separate keys for each function return
-// 	//appendDataToResponseObject(results);
-// 	if(allResults.length >= maxLength) {
-// 		response.send(allResults);
-// 		//done();
-// 	}
-// };
-
-exports.getTotalStudents = function(data, callback, done) {
-
-	var resultObj = {};
-
-	var query = 'SELECT SUM(undergraduates) AS underGraduates, SUM(graduates) AS graduates, SUM(faculty) AS faculty, SUM(other) AS other FROM tbl_lisd WHERE classDate >= "' + data.fromDate + '" AND classDate <= "' + data.toDate + '"';
-	database.query(query, function (error, results, fields) {
-	  
-	  if(error) {
-		console.log("Database error: " + error);
-	  }
-	  // TODO utilize resultObj
-	  callback(results,done); // TODO test results for db error?  ex. what is returned with a db error, is it an array?  does it have empty object?  Test and move to getDepartmentTotals()
-	});
-};
-
-exports.getTotalStudentsByDepartment = function(data, callback, done) {
-	callback({deptTotals: '18'},done);
-};
-
-// exports.getTotalStudentsByDepartment = function(data, callback, done) {
-
-// };
-// exports.getTotalClassesByDepartment = function(data, callback, done) {
-
-// };
-// exports.getTotalClassesByLocation = function(data, callback, done) {
-
-// };
-// exports.getTotalClassesByClassType = function(data, callback, done) {
-
-// };
 
