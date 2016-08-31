a1cb461611d51bdee9430de38250b3fa0c4d69e7
Jeff <you@example.com>
2016-08-31 15:35:23 -0600

    Add null date check to getAllStudents()

--- server/models/Aggregator.js
+++ server/models/Aggregator.js
@@ -8,15 +8,20 @@ var database = require('../lib/Database.js');
 // data:  If librarianID is present, get data for that librarian.  If byMonth is true, will aggregate results by monts.
 exports.getAllData = function(data, callback, done) {
 	
+	// Detect empty date values
+	if(data.fromDate.substring(0,9) == 'undefined' || data.toDate.substring(0,9) == 'undefined') {
+		callback({error: 'Aggregator says: no date specified'});
+	}
+
 	var allResults = [];
 	var queries = {
 		totalStudents : 'SELECT SUM(undergraduates) AS underGraduates, SUM(graduates) AS graduates, SUM(faculty) AS faculty, SUM(other) AS other FROM tbl_lisd WHERE classDate >= "' + data.fromDate + '" AND classDate <= "' + data.toDate + '"',
 		totalStudentsByDepartment: 'SELECT department, SUM(a.undergraduates) AS underGraduates, SUM(a.graduates) AS graduates, SUM(a.faculty) AS faculty, SUM(a.other) AS other FROM tbl_lisd a, tbl_lisdDepartment b WHERE a.departmentID = b.departmentID AND classDate BETWEEN "' + data.fromDate +'" AND "' + data.toDate +'" Group By b.department;'
 	};
 
-	// Build response object.  Send to controller once all data has been appended
+	// Build the response object.  Send it back to the controller once all data has been appended.
 	var appendDataToResponseObject = function(data) {
-		// If data is a DB error, send the error response.
+
 		for(var key in data) {
 			allResults[key] = data[key];
 		}
