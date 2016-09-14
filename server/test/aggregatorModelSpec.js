02e9a00b190944e0065be2cd3848e20a798a3e88
Jeff <you@example.com>
2016-09-14 16:32:57 -0600

    GC; add Class model

--- server/test/aggregatorModelSpec.js
+++ server/test/aggregatorModelSpec.js
@@ -5,21 +5,21 @@ var assert = require("chai").assert;
 var aggregatorModel = require("../models/Aggregator.js");
  
 describe("Aggregator", function(){
-    describe("#getTotalStudents()", function(){
+ //    describe("#getTotalStudents()", function(){
 
-   	    var data = {
-			fromDate: '2015-07-01',
-			toDate: '2016-06-30',
-			listByMonth: 0,
-			librarianID: 0
-		}
-		it("should return an array", function(done){
-	        aggregatorModel.getAllData(data, function(results, done) {
-	            assert.isArray(results);
-	            //console.log(results);
-	    	    done();
-	        }, done);
+ //   	    var data = {
+	// 		fromDate: '2015-07-01',
+	// 		toDate: '2016-06-30',
+	// 		listByMonth: 0,
+	// 		librarianID: 0
+	// 	}
+	// 	it("should return an array", function(done){
+	//         aggregatorModel.getAllData(data, function(results, done) {
+	//             assert.isArray(results);
+	//             //console.log(results);
+	//     	    done();
+	//         }, done);
 
-    	});
-	});
+ //    	});
+	// });
 });
