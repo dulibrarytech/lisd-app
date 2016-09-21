6b5f91012e042e0b0c0c76bf79cda4b1b89af314
Jeff <you@example.com>
2016-09-20 21:39:55 -0600

    addDocument function tests ok

--- server/test/classSpec.js
+++ server/test/classSpec.js
@@ -8,37 +8,34 @@ describe("Class", function(){
     describe("#addDocument()", function(){
 
     	// Get a test course number
-    	var randomCourseNum = Math.round(Math.random() * 100);
+    	var randomCourseNum = Math.round(Math.random() * 12345);
     	randomCourseNum = "test-" + randomCourseNum.toString();
 
-    	var testDoc = {
+    	// Test document to be inserted to the db
+        var testDoc = {
     		courseInfo: {
-    			className: "mocha 1",
+    			className: "MOCHA TEST - DELETE THIS RECORD",
     			courseNumber: randomCourseNum,
-    			instructor: "John Teacher",
-    			dateCreated: "2002-03-30"
+    			instructor: "Test Teacher",
+    			dateCreated: "2002-12-25"
     		},
     		enrollmentInfo: {
-
+                undergraduates: 12,
+                graduates: 30,
+                faculty: 4,
+                other: 3
     		},
-    		associatedLibrarians: ["111"],
-    		location: ["222"],
-    		department: ["333"],
-    		classType: ["444"],
-    		comments: [{}]
+    		associatedLibrarians: ["57e1f8bfadb181f4aec01010"],
+    		location: ["57e1f8bfadb181f4aec01011"],
+    		department: ["57e1f8bfadb181f4aec01100"],
+    		classType: ["57e1f8bfadb181f4aec01111"],
+    		comments: [{name: "test", text:"test comment"}]
     	};
-
-    	// it("should return an object", function(done){
-	    //     classModel.addDocument(testDoc, function(results) {
-	    //         assert.isObject(results);
-	    //         console.log(results);
-	    // 	    done();
-	    //     }, done);
-    	// });
 
     	it("should return a status of ok", function(done){
 	        classModel.addDocument(testDoc, function(results) {
 	            expect(results.status).to.equal('ok');
+                if(results.message) {console.log("MESSAGE: " + results.message);}
 	    	    done();
 	        }, done);
     	});
