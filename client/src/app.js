59de7f8f5831046d38190e60687e71f35aa9e6bf
Jeff <you@example.com>
2016-09-27 10:37:50 -0600

    Rename users component

--- client/src/app.js
+++ client/src/app.js
@@ -4,7 +4,7 @@ export class App {
     config.title = 'Library Instruction Statistics Database';
     config.map([
       { route: ['', 'home'], name: 'home',      moduleId: 'components/home/home',      nav: true, title: 'Home' },
-      { route: ['users', 'users'], name: 'users',      moduleId: 'components/users/users',      nav: true, title: 'Users' },
+      { route: ['admin', 'admin'], name: 'admin',      moduleId: 'components/admin/admin',      nav: true, title: 'Admin' },
       { route: ['entryform', 'entryform'], name: 'entryform',      moduleId: 'components/entryform/entryform',      nav: true, title: 'Entry Form' }
     ]);
 
