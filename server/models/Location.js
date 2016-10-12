a974ad3b63c2c8a046e6e4e210e0c2b1c2e476d7
Jeff <you@example.com>
2016-10-12 17:01:06 -0600

    Add all model classes

---
+++ server/models/Location.js
@@ -0,0 +1,18 @@
+'use strict'
+
+module.exports = (function() {
+
+
+
+
+
+
+	return {
+		addDocument: function(doc,callback) {
+			addDocument(doc,callback);
+		},
+		getLocations: function(callback) {
+			getLocations(callback);
+		}
+	};
+})()
