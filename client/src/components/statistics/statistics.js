fd7f6a457f1af265e116bf5bb01564c2507430e8
Jeff <you@example.com>
2017-01-25 15:20:19 -0700

    Add type dropdown

--- client/src/components/statistics/statistics.js
+++ client/src/components/statistics/statistics.js
@@ -1,5 +1,5 @@
 export class Statistics {
-    selectedOptions = [];
 
-    someOptions = ['Option 1', 'Option 2', 'Option 3'];
+    selectedOptions = [];
+    searchType = ["All Statistics", "Librarian Statistics", 'Class Data'];
 }
