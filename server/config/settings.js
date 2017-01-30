a0ccf6106133b89eb13ff1e36fbd9c4d66f3e0b1
Jeff <you@example.com>
2017-01-30 16:22:47 -0700

    Add quarter and academic year data to settings; add getDate() function to agg controller

--- server/config/settings.js
+++ server/config/settings.js
@@ -4,7 +4,19 @@ var settings = {
 
 	server: {
 		fiscalYearStart: 		"07-01",
-		fiscalYearEnd: 			"06-30"
+		fiscalYearEnd: 			"06-30",
+
+		academicYearStart: 		"09-01",
+		academicYearEnd: 		"08-30",
+
+		quarter1Start: 			"09-01",
+		quarter1End: 			"12-15",
+		quarter2Start: 			"01-01",
+		quarter2End: 			"03-15",
+		quarter3Start: 			"03-16",
+		quarter3End: 			"06-15",
+		quarter4Start: 			"06-16",
+		quarter4End: 			"8-30"
 	},
 
 	test: {
