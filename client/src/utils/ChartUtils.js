1b3819c0ffd0057e108eafb7e69e7b8a92057edc
Jeff <you@example.com>
2017-03-12 13:40:49 -0600

    Separate chart config functions and settings into separate class

---
+++ client/src/utils/ChartUtils.js
@@ -0,0 +1,78 @@
+export class ChartUtils {
+
+	constructor() {
+		 // Chart configuration
+        this.chartOptions = {
+
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero:true
+                    }
+                }],
+                yAxes: [{
+                    ticks: {
+                        fontSize: 30
+                    }
+                }]
+            }
+        }
+	}
+
+	getSingleChartConfig(resultListType, statisticsType) {
+		var config = {};
+		if(resultListType == "Total") {
+
+            config = {
+
+              type: 'bar',
+              data: {
+                labels: ['Classes by Year'],
+                datasets: [{
+                  label: 'Number of Classes',
+                  data: [12],
+                  backgroundColor: "rgba(153,255,51,0.4)"
+                }]
+              },
+              options: this.chartOptions
+            }
+        }
+        else if(resultListType == "Month") {
+
+            config = {
+
+              type: 'bar',
+              data: {
+                labels: ['Classes by Month'],
+                datasets: [{
+                  label: 'Number of Classes',
+                  data: [12],
+                  backgroundColor: "rgba(153,255,51,0.4)"
+                }]
+              },
+              options: this.chartOptions
+            }
+        }
+        else if(resultListType == "fdfd") {
+            config = {
+
+              type: 'bar',
+              data: {
+                labels: ['Classes by Quarter'],
+                datasets: [{
+                  label: 'Number of Classes',
+                  data: [12],
+                  backgroundColor: "rgba(153,255,51,0.4)"
+                }]
+              },
+              options: this.chartOptions
+            }
+        }
+
+        return config;
+	}
+
+	getSubsortChartConfig(resultListType, statisticsType) {
+
+	}
+}
