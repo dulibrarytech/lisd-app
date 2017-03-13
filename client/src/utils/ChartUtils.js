c68c9ce4991c41243436ecc9298e53b186683d44
Jeff <you@example.com>
2017-03-13 15:59:43 -0600

    Add chart function for student single display

--- client/src/utils/ChartUtils.js
+++ client/src/utils/ChartUtils.js
@@ -19,6 +19,8 @@ export class ChartUtils {
                 }]
             }
         }
+
+        this.numOfStudentTypes = 4;	// settings
 	}
 
 	renderClassSingleChart(labelArray, dataArray) {	// This does not know about timeframes.  Sort external to this function?
@@ -56,6 +58,47 @@ export class ChartUtils {
 	}
 
 	renderStudentSingleChart(labelArray, dataArray) {
-		
+
+		var colorIndex = 0;
+		var barColors = ['#C1F3F9', '#B9DBA6', '#F2E4C7', '#DBAAAB', '#CAC1F9'];	// Kuler scheme
+
+		var dataSetArray = [], newArray = [];
+		var dataSet = {};
+		var value;
+		for(var i=0; i<this.numOfStudentTypes; i++) {
+			dataSet = {};
+			newArray = [];
+			for(var j in dataArray) {
+				newArray[j] = dataArray[j][i];
+			}
+			dataSet['fillColor'] = barColors[i];
+			dataSet['data'] = newArray;
+			dataSetArray.push(dataSet);		
+		}
+
+		var config = {
+
+		    labels: labelArray,
+		    datasets: dataSetArray
+		};
+
+        var ctx = document.getElementById("results-chart").getContext("2d");
+		var myBar = new Chart(ctx).Bar(config, {
+		    showTooltips: false,
+		    onAnimationComplete: function () {
+
+		        var ctx = this.chart.ctx;
+		        ctx.font = this.scale.font;
+		        ctx.fillStyle = this.scale.textColor
+		        ctx.textAlign = "center";
+		        ctx.textBaseline = "bottom";
+
+		        this.datasets.forEach(function (dataset) {
+		            dataset.bars.forEach(function (bar) {
+		                ctx.fillText(bar.value, bar.x, bar.y - 5);
+		            });
+		        })
+		    }
+		});
 	}
 }
