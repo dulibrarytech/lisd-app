6cac08e9c689dc06b7184c31d77aa1f6e7e6da5c
Jeff <you@example.com>
2017-03-12 19:49:40 -0600

    Move chart config out of chart utils class, pass in dynamic config data

--- client/src/utils/ChartUtils.js
+++ client/src/utils/ChartUtils.js
@@ -1,3 +1,5 @@
+
+
 export class ChartUtils {
 
 	constructor() {
@@ -19,60 +21,19 @@ export class ChartUtils {
         }
 	}
 
-	renderClassSingleChart(resultData,  resultListType, timeframe) {	// This does not know about timeframes.  Sort external to this function?
-		
-		var months = [], quarters = [];
-		var data = [];
+	renderClassSingleChart(labelArray, dataArray) {	// This does not know about timeframes.  Sort external to this function?
 
-		console.log("DEV3:");
-		console.log(resultData.year.total);
+        var config = {
 
-		var config = {};
-		if(resultListType == "Total") {
-
-			data = [resultData.year.total];
-
-            config = {
-			    labels: ["Total Classes"],
-			    datasets: [
-			        {
-			            fillColor: "#79D1CF",
-			            strokeColor: "#79D1CF",
-			            data: data
-			        }
-			    ]
-			};
-        }
-        else if(resultListType == "Month") {
-
-        	// Get months and data from resultObject
-
-            config = {
-			    labels: months,
-			    datasets: [
-			        {
-			            fillColor: "#79D1CF",
-			            strokeColor: "#79D1CF",
-			            data: data
-			        }
-			    ]
-			};
-        }
-        else if(resultListType == "Quarter") {
-
-        	// Get quarters and data from resultObject
-
-            config = {
-			    labels: quarters,
-			    datasets: [
-			        {
-			            fillColor: ["#79D1CF"],
-			            strokeColor: "#79D1CF",
-			            data: data
-			        }
-			    ]
-			};
-        }
+		    labels: labelArray,
+		    datasets: [
+		        {
+		            fillColor: "#79D1CF",
+		            strokeColor: "#79D1CF",
+		            data: dataArray
+		        }
+		    ]
+		};
 
         var ctx = document.getElementById("results-chart").getContext("2d");
 		var myBar = new Chart(ctx).Bar(config, {
@@ -93,6 +54,78 @@ export class ChartUtils {
 		    }
 		});
 	}
+
+	// renderClassSingleChart(resultData,  resultListType, timeframe) {	// This does not know about timeframes.  Sort external to this function?
+		
+	// 	var months = [], quarters = [];
+	// 	var data = [];
+
+	// 	var config = {};
+	// 	if(resultListType == "Total") {
+
+	// 		data = [resultData.year.total];
+
+ //            config = {
+	// 		    labels: ["Total Classes"],
+	// 		    datasets: [
+	// 		        {
+	// 		            fillColor: "#79D1CF",
+	// 		            strokeColor: "#79D1CF",
+	// 		            data: data
+	// 		        }
+	// 		    ]
+	// 		};
+ //        }
+ //        else if(resultListType == "Month") {
+
+ //        	// Get months and data from resultObject
+
+ //            config = {
+	// 		    labels: months,
+	// 		    datasets: [
+	// 		        {
+	// 		            fillColor: "#79D1CF",
+	// 		            strokeColor: "#79D1CF",
+	// 		            data: data
+	// 		        }
+	// 		    ]
+	// 		};
+ //        }
+ //        else if(resultListType == "Quarter") {
+
+ //        	// Get quarters and data from resultObject
+
+ //            config = {
+	// 		    labels: quarters,
+	// 		    datasets: [
+	// 		        {
+	// 		            fillColor: ["#79D1CF"],
+	// 		            strokeColor: "#79D1CF",
+	// 		            data: data
+	// 		        }
+	// 		    ]
+	// 		};
+ //        }
+
+ //        var ctx = document.getElementById("results-chart").getContext("2d");
+	// 	var myBar = new Chart(ctx).Bar(config, {
+	// 	    showTooltips: false,
+	// 	    onAnimationComplete: function () {
+
+	// 	        var ctx = this.chart.ctx;
+	// 	        ctx.font = this.scale.font;
+	// 	        ctx.fillStyle = this.scale.textColor
+	// 	        ctx.textAlign = "center";
+	// 	        ctx.textBaseline = "bottom";
+
+	// 	        this.datasets.forEach(function (dataset) {
+	// 	            dataset.bars.forEach(function (bar) {
+	// 	                ctx.fillText(bar.value, bar.x, bar.y - 5);
+	// 	            });
+	// 	        })
+	// 	    }
+	// 	});
+	// }
 
 	getSubsortChartConfig(resultListType, statisticsType) {
 
