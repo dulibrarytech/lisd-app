4c0c0e8c9cc37c18d81f9a6b9368713aed23e4cd
Jeff <you@example.com>
2017-03-29 20:41:34 -0600

    Complete student charts with upgraded library

--- client/src/utils/ChartUtils.js
+++ client/src/utils/ChartUtils.js
@@ -20,8 +20,14 @@ export class ChartUtils {
             }
         }
 
-        this.numOfStudentTypes = 4;	// settings
+        //this.numOfStudentTypes = 4;	// settings
+        this.studentTypes = ["Undergraduates", "Graduates", "Faculty", "Other"]
         this.barColorScheme = ['#C1F3F9', '#B9DBA6', '#F2E4C7', '#DBAAAB', '#CAC1F9'];	// Kuler scheme
+        this.barBorderColorScheme = ['#769598', '#687A5C', '#918978', '#7A5F60', '#7B7698'];
+	}
+
+	getMaxOfArray(numArray) {
+	  return Math.max.apply(null, numArray);
 	}
 
 	renderClassSingleChart(labelArray, dataArray) {	// This does not know about timeframes.  Sort external to this function?
@@ -46,7 +52,6 @@ export class ChartUtils {
 		console.log(data);
 
 		var ctx = document.getElementById("results-chart");
-
 		var myChart = new Chart(ctx, {
 		    type: 'bar',
 		    data: data,
@@ -79,60 +84,88 @@ export class ChartUtils {
 
 	renderStudentSingleChart(labelArray, dataArray) {
 
-		var colorIndex = 0;
-		var barColors = this.barColorScheme;
+		console.log("---RENDERING---");
+
+		var barWidthPct = 0.9;
+		// if(dataArray.length == 1) {
+		// 	barWidthPct = 0.2
+		// }
 
 		var dataSetArray = [], newArray = [];
 		var dataSet = {};
 		var value;
-		for(var i=0; i<this.numOfStudentTypes; i++) {
+		for(var i=0; i<this.studentTypes.length; i++) {
 			dataSet = {};
 			newArray = [];
+
 			for(var j in dataArray) {
 				newArray[j] = dataArray[j][i];
 			}
-
-			// dataSet['fillColor'] = barColors[i];
-			// dataSet['data'] = newArray;
+			dataSet['data'] = newArray;
+			dataSet['label'] = this.studentTypes[i];
+			dataSet['backgroundColor'] = this.barColorScheme[i];
+			dataSet['borderColor'] = this.barBorderColorScheme[i];
 
 			dataSetArray.push(dataSet);		
 		}
 
-
-
-
-
-
-
-
-
-
-
-		var config = {
+		var data = {
 
 		    labels: labelArray,
 		    datasets: dataSetArray
 		};
 
-        var ctx = document.getElementById("results-chart").getContext("2d");
-		var myBar = new Chart(ctx).Bar(config, {
-		    showTooltips: false,
-		    // tooltipTemplate: "<%if (label){%><%=label%>: <%}%><%= value %>kb",
-		    onAnimationComplete: function () {
+		// Find largest value in array, to set Y ticks
+		var maxVal = 0, tickInc = 1;
+		for(var i in dataSetArray) {
+			for(var j in dataArray[i]) {
+				if(dataArray[i][j] > maxVal) {
+					maxVal = dataArray[i][j];
+				}
+			}
+		}
+		// If odd number, make it even so no non-integers are displayed
+		if(tickInc % 2 == 0) {
+			tickInc = maxVal / 8;
+		}
+		else {
+			tickInc = (maxVal+1) / 8;
+		}
 
-		        var ctx = this.chart.ctx;
-		        ctx.font = this.scale.font;
-		        ctx.fillStyle = this.scale.textColor
-		        ctx.textAlign = "center";
-		        ctx.textBaseline = "bottom";
 
-		        this.datasets.forEach(function (dataset) {
-		            dataset.bars.forEach(function (bar) {
-		                ctx.fillText(bar.value, bar.x, bar.y - 5);
-		            });
-		        })
+		var ctx = document.getElementById("results-chart");
+		var myChart = new Chart(ctx, {
+		    type: 'bar',
+		    data: data,
+		    options: {
+		        scales: {
+		            yAxes: [{
+		                ticks: {
+		                    beginAtZero: true,
+		                    fontSize: 25,
+		                    stepSize : tickInc,
+		                }
+		            }],
+		            xAxes: [{
+		                // Change here
+		            	barPercentage: barWidthPct,
+		            	ticks: {
+		                    fontSize: 25
+		                }
+		            }]
+		        },
+	            legend: {
+	            	display: true,
+	            	position: 'bottom',
+	            	labels: {
+	            		fontSize: 25,
+	            		padding: 40
+	            	}
+	            },
+	            tooltips: {
+	            	enabled: false
+	            }
 		    }
 		});
-
 	}
 }
