e39a193a1a1e6d0fedca705b436919ed77bb2eb5
Jeff <you@example.com>
2017-04-12 14:24:49 -0600

    Complete totals upgrade, Agg and views

--- client/src/utils/SubsortTotalsValueConverter.js
+++ client/src/utils/SubsortTotalsValueConverter.js
@@ -1,14 +1,30 @@
+// export class SubsortTotalsValueConverter {
+    
+//     // Convert month key from month number to month name
+//     toView(subsortTotalsObject, index) {
+
+//         let total = 0;
+
+//         for(var key in index) {
+//             total = subsortTotalsObject[key];
+//         }
+        
+//         return total;
+//     }
+// }
+
 export class SubsortTotalsValueConverter {
     
     // Convert month key from month number to month name
     toView(subsortTotalsObject, index) {
 
         let total = 0;
+        let totalsObj = {};
 
         for(var key in index) {
-            total = subsortTotalsObject[key];
+            totalsObj = subsortTotalsObject[key];
         }
-        
-        return total;
+
+        return totalsObj;
     }
 }
