19bf1f9f7d6e7e16461be6a63e76562ed3081eab
Jeff <you@example.com>
2017-09-27 14:10:37 -0600

    Fix display bug - remove 'total' data resulting in errant column

--- client/src/utils/SubsortTotalsValueConverter.js
+++ client/src/utils/SubsortTotalsValueConverter.js
@@ -1,21 +1,7 @@
-// export class SubsortTotalsValueConverter {
-    
-//     // Convert month key from month number to month name
-//     toView(subsortTotalsObject, index) {
 
-//         let total = 0;
-
-//         for(var key in index) {
-//             total = subsortTotalsObject[key];
-//         }
-        
-//         return total;
-//     }
-// }
 
 export class SubsortTotalsValueConverter {
     
-    // Convert month key from month number to month name
     toView(subsortTotalsObject, index) {
 
         let total = 0;
