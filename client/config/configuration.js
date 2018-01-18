0595d152361ea92a4194db4a76a40779afb25e1b
Jeff <you@example.com>
2018-01-18 15:58:02 -0700

    Update class edit button visibility logic

--- client/config/configuration.js
+++ client/config/configuration.js
@@ -1,7 +1,7 @@
 export class Configuration {  
   constructor(){
       this.baseUrl = 'http://localhost:9004/';
-      this.environment = 'development';
+      this.environment = 'production';
 
       this.session = {
       	data: null,
