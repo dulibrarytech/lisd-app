4b7842d982eb99ecde618caba891a2eac6a1cda5
Jeff Rynhart <jeff.rynhart@du.edu>
2024-05-20 11:27:47 -0600

    Refactored database functions in User model and Librarian model

--- server/models/Librarian.js
+++ server/models/Librarian.js
@@ -2,123 +2,111 @@
 
 module.exports = (function() {
 
-	var mongo = require('mongodb');
 	var database = require('../util/database.js');
-	var ObjectId = require('mongodb').ObjectID;
-	var collection;
+	var collection = {};
 
-	database.connect(function(db) {
-		//var db = database.connection();
-		collection = db.collection('lisd_librarian');
-		// DEV
-		//console.log("Librarian model connected to db...");
-	});
+	// TEST single connection in bootstrap
+	// collection = database.connection().collection('lisd_librarian');
+
+	console.log("Librarian model connecting to database..");
+	database.connect()
+		.then((db) => {
+			if(!db) throw "Librarian database connection failed";
+			else {
+				console.log(`Librarian model connected to database: ${db}`);
+				collection = db.collection('lisd_librarian');
+			}
+		})
+		.catch(console.error)
+		.finally(() => {
+			// if(database) database.closeConnection();
+		});
 	
-	var addLibrarian = function(data, callback) {
-
+	var addLibrarian = async function(data, callback) {
 		try {
-			// Insert the document
-		    collection.insertOne(data, function(err, result) {
-			    if(err) {
-			    	console.log("Error: " + err);
-			    	callback(false);
-			    }
-			    else {
-			    	console.log("Added librarian ", result.ops[0]._id);
-			    	callback(true);
-			    }
-			});
+			let result = await collection.insertOne(data);
+			console.log("Added librarian ", result.insertedId.toString());
+			callback(true);
+
 		} catch (e) {
 			console.log("Error: " + e);
 			callback(false);
 		};
 	};
 
-	var getList = function(callback) {
+	var getList = async function(callback) {
 		var results = {
 			librarian: {}
 		};
 		
 		try {
-			var cursor = collection.find({ "isActive": true }, {"_id": 1, "firstname": 1, "lastname": 1}).sort({ lastname: 1 });
-	        cursor.each(function(err, item) {
-	        	if(item != null) {
-	        		var nameStr = item.lastname + ", " + item.firstname;
-	        		results.librarian[item._id] = nameStr;
-	        	}
-	        	else {
-	        		callback({status: "ok", message: "Ok", data: results});
-	        	}
-	        });
+			var cursor = await collection.find({ "isActive": true }, {"_id": 1, "firstname": 1, "lastname": 1}).sort({ lastname: 1 }).toArray();
+
+			for(let item of cursor) {
+				var nameStr = item.lastname + ", " + item.firstname;
+				results.librarian[item._id] = nameStr;
+			}
+
+			callback({status: "ok", message: "Ok", data: results});
 		}
 		catch (e) {
 			callback({status: "error", message: "Error: " + e});
 		}
 	};
 
-	var getAll = function(callback) {
+	var getAll = async function(callback) {
 		var results = {
 			librarian: {}
 		};
 
 		try {
-			var cursor = collection.find({}, {"_id": 1, "firstname": 1, "lastname": 1}).sort({ lastname: 1 });
-	        cursor.each(function(err, item) {
-	        	if(item != null) {
-					var nameStr = item.lastname + ", " + item.firstname;
-	        		results.librarian[item._id] = nameStr;
-	        	}
-	        	else {
-	        		callback({status: "ok", message: "Ok", data: results});
-	        	}
-	        });
+			var cursor = await collection.find({}, {"_id": 1, "firstname": 1, "lastname": 1}).sort({ lastname: 1 }).toArray();
+
+			for(let item of cursor) {
+				var nameStr = item.lastname + ", " + item.firstname;
+				results.librarian[item._id] = nameStr;
+			}
+
+			callback({status: "ok", message: "Ok", data: results});
 		}
 		catch (e) {
 			callback({status: "error", message: "Error: " + e});
 		}
 	};
 
-	var findByUserID = function(userID, callback) {
+	var findByUserID = async function(userID, callback) {
 		var librarianID = "";
 		var query = {};
+
 		query["userID"] = userID.toString();
+
 		try {
-			var cursor = collection.findOne(query, function(err, item) {
-		    	if(err) {
-		    		console.log("Error: " + err);
-		    		callback(librarianID);
-		    	}
-		    	if(item != null) {
-		    		librarianID = item._id;
-		    	}
-		    	else {
-		    		console.log("Librarian found");
-		    	}
-		    	callback(librarianID)
-		    });
+			let item = await collection.findOne(query);
+
+			if(item != null) {
+				librarianID = item._id;
+			}
+			else {
+				console.log("Librarian not found");
+			}
 		}
 		catch (e) {
 			console.log("Error: " + e);
-			callback(librarianID);
 		}
+
+		callback(librarianID);
 	};
 
-	var setLibrarianInactive = function(userID, callback) {
+	var setLibrarianInactive = async function(userID, callback) {
 		try {
 			var librarianData = {
 				"userID": "",
 				"isActive": false
 			}
-			// Insert the document
-		    collection.updateOne({"userID": userID}, {$set: librarianData}, function(err, result) {
-			    if(err) {
-			    	console.log("Error: " + err);
-			    	callback(false);
-			    }
-			    else {
-			    	callback(true)
-			    }
-			});
+
+			let response = await collection.updateOne({"userID": userID}, {$set: librarianData});
+			callback(true);
+
 		} catch (e) {
 			console.log("Error: " + e);
 			callback(false);
--- server/models/User.js
+++ server/models/User.js
@@ -1,238 +1,248 @@
 var database = require('../util/database.js');
-var settings = require("../config/settings");
-var encryptor = require('simple-encryptor')(settings.cryptKey);
+var ObjectId = require('mongodb').ObjectId; 
+var collection = {};
 
-var ObjectId = require('mongodb').ObjectID;
-var collection;
-database.connect(function(db) {
-	//var db = database.connection();
-	collection = db.collection('lisd_user');
-});
-
-exports.validateLisdUser = function(username) {
-
-	return new Promise(function(fulfill, reject) {
-		try {
-			//var foundUser = false;
-			var cursor = collection.find();
-	        cursor.each(function(err, item) {
-
-	        	if(item != null) {
-	        		if(item.username == username || item.duid == username) {
-
-		        		var userObject = {
-		        			userID: item._id,
-		        			fname: item.firstname,
-		        			lname: item.lastname,
-		        			role: item.role,
-		        			username: item.username
-		        		};
-		        		fulfill(userObject);
-		        	}
-		        	else {
-		        		//fulfill(false);
-		        	}
-	        	}
-	        	else {
-	        		fulfill(false);
-	        	}
-	        });
+console.log("User model connecting to database..");
+database.connect()
+	.then((db) => {
+		if(!db) throw "User database connection failed";
+		else {
+			console.log(`User model connected to database: ${db}`);
+			collection = db.collection('lisd_user');
 		}
-		catch (e) {
-			console.log("Error: " + e);
-			fulfill(false);
-		}
+	})
+	.catch(console.error)
+	.finally(() => {
+		// if(database) database.closeConnection();
 	});
 
-	//return true;
+exports.validateLisdUser = async function(username) {
+	
+	try {
+		var cursor = await collection.find().toArray();
+		console.log("TEST User::validateLisdUser(): cursor:", cursor)
+		for(let item of cursor) { // DEV keep as is, cursor[0] must be the item
+			if(item != null) {
+				if(item.username == username || item.duid == username) {
+
+					var userObject = {
+						userID: item._id,
+						fname: item.firstname,
+						lname: item.lastname,
+						role: item.role,
+						username: item.username
+					};
+
+					return userObject;
+				}
+				// else {
+				// 	return false; // unused?
+				// }
+			}
+			else {
+				return false;
+			}
+		};
+	}
+	catch (e) {
+		console.log("Error: " + e);
+		return false;
+	}
 };
 
-exports.findDUID = function(username) {
+exports.findDUID = async function(username) {
 
-	return new Promise(function(fulfill, reject) {
+	//return new Promise(function(fulfill, reject) {
 
 		// If numeric is used as username, assume it is DUID and use that
 		if(isNaN(username) === false) {
-			fulfill(username);
+			return username;
 		}
 
 		try {
-			var cursor = collection.find({"username": username});
-	        cursor.each(function(err, item) {
+			var cursor = await collection.find({"username": username}).toArray();
+			console.log("TEST User::findDUID() cursor returned:", cursor)
+	        for(let item of cursor) { // DEV convert to first [] index access
 
 	        	if(item != null) {
-	        		fulfill(item.duid);
+	        		return item.duid;
 	        	}
 	        	else {
-	        		fulfill(false);
+	        		return false;
 	        	}
-	        });
+	        };
 		}
 		catch (e) {
 			console.log("Error: " + e);
-			fulfill(false);
+			return false;
 		}
-	});
+	//});
 };
 
-exports.getAllUsers = function() {
+exports.getAllUsers = async function() {
 
-	return new Promise(function(fulfill, reject) {
+	//return new Promise(function(fulfill, reject) {
 
 		var users = [];
 
 		try {
-			var cursor = collection.find({});
-	        cursor.each(function(err, item) {
+			var users = await collection.find({}).toArray();
+			console.log("TEST User::getAllUsers() users returned:", users)
 
-	        	if(item != null) {
-	        		users.push(item);
-	        	}
-	        	else {
-	        		fulfill(users);
-	        	}
-	        });
+			return users;
+	        // for(let item of cursor) {
+
+	        // 	if(item != null) {
+	        // 		users.push(item);
+	        // 	}
+	        // 	else {
+	        // 		return users;
+	        // 	}
+	        // };
 		}
 		catch (e) {
 			console.log("Error: " + e);
-			fulfill(false);
+			return false;
 		}
-	});
+	//});
 };
 
-exports.insertDuid = function(duid, lastName) {
-
-	return new Promise(function(fulfill, reject) {
+exports.insertDuid = async function(duid, lastName) {
+	console.log("TEST User::insertDuid(): duid/lastname:", duid, lastname)
+	//return new Promise(function(fulfill, reject) {
 		try {
 			// Insert the document
-		    collection.updateOne({lastname:lastName}, {$set: {duid:duid}}, function(err, result) {
-			    if(err) {
-			    	console.log("Error: " + err);
-			    	reject(false);
-			    }
-			    else {
-			    	fulfill(duid)
-			    }
-			    // db.close();
-			});
+		    await collection.updateOne({lastname:lastName}, {$set: {duid:duid}});
+			    //if(err) {
+			    	//console.log("Error: " + err);
+			    	//return false;
+			    //}
+			    //else {
+					console.log("User " + duid + " added");
+			    	return duid;
+			    //}
+			//});
 		} catch (e) {
 			console.log("Error: " + e);
-			fulfill(false);
+			return false;
 		};
-	});
+	//});
 };
 
-exports.getUserData = function(userID) {
+exports.getUserData = async function(userID) {
 
-	return new Promise(function(fulfill, reject) {
+	//return new Promise(function(fulfill, reject) {
 
 		var userData = {};
 		try {
-			var cursor = collection.find({ "_id": ObjectId(userID) });
-	        cursor.each(function(err, item) {
-	        	if(item != null) {
-	        		userData = item;
-	        	}
-	        	else {
-	        		fulfill(userData);
-	        	}
-	        });
+			var cursor = await collection.find({ "_id": new ObjectId(userID) }).toArray();
+			console.log("TEST User::getUserData() cursor returned:", cursor)
+	        for(let item of cursor) { // DEV convert to first [] index access
+	        	// if(item != null) {
+	        	// 	userData = item;
+	        	// }
+	        	// else {
+	        		return item; // DEV
+	        	// }
+	        };
 		}
 		catch (e) {
 			console.log("Error: " + e);
-			fulfill(false);
+			return false;
 		}
-	});
+	//});
 };
 
-exports.updateUserData = function(userID, userData) {
-
-	return new Promise(function(fulfill, reject) {
+exports.updateUserData = async function(userID, userData) {
+	console.log("TEST User::updateUserData()")
+	//return new Promise(function(fulfill, reject) {
 		try {
 			// Insert the document
-		    collection.updateOne({"_id": ObjectId(userID)}, {$set: userData}, function(err, result) {
-			    if(err) {
-			    	console.log("Error: " + err);
-			    	fulfill(false);
-			    }
-			    else {
+		    await collection.updateOne({"_id": new ObjectId(userID)}, {$set: userData});
+			    // if(err) {
+			    // 	console.log("Error: " + err);
+			    // 	return false;
+			    // }
+			    // else {
 			    	console.log("User " + userID + " updated");
-			    	fulfill(true)
-			    }
-			});
+			    	return true;
+			//     }
+			// });
 		} catch (e) {
 			console.log("Error: " + e);
-			fulfill(false);
+			return false;
 		};
-	});
+	//});
 };
 
-exports.addUserData = function(userData) {
-
-	return new Promise(function(fulfill, reject) {
+exports.addUserData = async function(userData) {
+	console.log("TEST User::addUserData()")
+	//return new Promise(function(fulfill, reject) {
 		try {
 			// Insert the document
-		    collection.insertOne(userData, function(err, result) {
-			    if(err) {
-			    	console.log("Error: " + err);
-			    	fulfill(false);
-			    }
-			    else {
-			    	console.log("Added user " + result.ops[0]._id);
-			    	fulfill(result.ops[0]._id.toString());
-			    }
-			});
+		    let result = await collection.insertOne(userData);
+			console.log("TEST result:", result)
+			    // if(err) {
+			    // 	console.log("Error: " + err);
+			    // 	return false;
+			    // }
+			    // else {
+			    	console.log("Added user " + result.insertedId.toString());
+			    	return result.insertedId.toString();
+			//     }
+			// });
 		} catch (e) {
 			console.log("Error: " + e);
-			fulfill(false);
+			return false;
 		};
-	});
+	//});
 };
 
-exports.removeUserData = function(userID) {
-
-	return new Promise(function(fulfill, reject) {
+exports.removeUserData = async function(userID) {
+	console.log("TEST User::removeUserData()")
+	//return new Promise(function(fulfill, reject) {
 		try {
 			// Insert the document
-		    collection.deleteOne({ "_id": ObjectId(userID)}, function(err, result) {
-			    if(err) {
-			    	console.log("Error: " + err);
-			    	fulfill(false);
-			    }
-			    else {
-			    	console.log("User " + userID + " removed " + result);
-			    	fulfill(true);
-			    }
-			});
+		    await collection.deleteOne({ "_id": new ObjectId(userID)});
+			    // if(err) {
+			    // 	console.log("Error: " + err);
+			    // 	return false;
+			    // }
+			    // else {
+			    	console.log("User " + userID + " removed");
+			    	return true;
+			//     }
+			// });
 		} catch (e) {
 			console.log("Error: " + e);
-			fulfill(false);
+			return false;
 		};
-	});
+	//});
 };
 
-exports.findUserByName = function(firstName, lastName) {
-	return new Promise(function(fulfill, reject) {
-
+exports.findUserByName = async function(firstName, lastName) {
+	//return new Promise(function(fulfill, reject) {
+		
 		try {
-			var cursor = collection.find({ "firstname" : firstName, "lastname" : lastName });
-	        cursor.each(function(err, item) {
+			var cursor = await collection.find({ "firstname" : firstName, "lastname" : lastName }).toArray();
+			console.log("TEST User::findUserByName() cursor returned:", cursor)
+	        for(let item of cursor) { // DEV convert to first [] index access
 	        	if(item != null) {
 	        		console.log("User exists");
-	        		fulfill(true);
-	        		return false;
+	        		return true;
+	        		//return false;
 	        	}
 	        	else {
 	        		console.log("User does not exist");
-	        		fulfill(false);
+	        		return false;
 	        	}
-	        });
+	        };
 		}
 		catch (e) {
 			console.log("Error: " + e);
-			fulfill(false);
+			return false;
 		}
-	});
+	//});
 };
 
 
