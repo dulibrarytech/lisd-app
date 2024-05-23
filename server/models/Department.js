6a763a66ed04be7f19955f5b3e3c5c3bfd77b87d
Jeff Rynhart <jeff.rynhart@du.edu>
2024-05-23 14:33:23 -0600

    Refactored database functions in Location model and Department model

--- server/models/Department.js
+++ server/models/Department.js
@@ -1,130 +1,122 @@
 'use strict'
 
-var database = require('../util/database.js');
-var collection;
-var ObjectId = require('mongodb').ObjectID;
+module.exports = (function() {
 
-database.connect(function(db) {
-	//var db = database.connection();
-	collection = db.collection('lisd_department');
-	// DEV
-	//console.log("Department model connected to db...");
-});
+	var database = require('../util/database.js');
+	var collection = {};
+	var ObjectId = require('mongodb').ObjectId;
 
-exports.getList = function(callback) {
-	var results = {
-		department: {}
-	};
-
-	try {
-		var cursor = collection.find({ "isActive": true }, {"_id": 1, "name": 1}).sort({name:1});
-        cursor.each(function(err, item) {
-        	if(item != null) {
-        		results.department[item._id] = item.name;
-        	}
-        	else {
-        		callback({status: "ok", message: "Ok", data: results});
-        	}
-        });
-	}
-	catch (e) {
-		callback({status: "error", message: "Error: " + e});
-	}
-};
-
-exports.getAll = function(callback) {
-	try {
-		var results = [];
-		var cursor = collection.find({}, {"_id": 1, "name": 1, "isActive": 1}).sort({name:1});
-        cursor.each(function(err, item) {
-        	if(item != null) {
-        		results.push(item);
-        	}
-        	else {
-        		callback({status: "ok", message: "Ok", data: results});
-        	}
-        });
-	}
-	catch (e) {
-		callback({status: "error", message: "Error: " + e});
-	}
-};
-
-exports.getData = function(id, callback) {
-
-	try {
-		var results = [];
-		var cursor = collection.find({ "_id": ObjectId(id) }, {"_id": 0, "name": 1, "isActive": 1});
-        cursor.each(function(err, item) {
-        	if(item != null) {
-        		results.push(item);
-        	}
-        	else {
-        		callback({status: "ok", message: "Ok", data: results});
-        	}
-        });
-	}
-	catch (e) {
-		callback({status: "error", message: "Error: " + e});
-	}
-};
-
-exports.updateData = function(id, data, callback) {
-
-	try {
-		// Insert the document
-	    collection.updateOne({"_id": ObjectId(id)}, {$set: data}, function(err, results) {
-		    if(err) {
-		    	console.log("Error: " + err);
-		    	callback({status: "error", message: "Error: " + e});
-		    }
-		    else {
-		    	console.log("Department " + id + " updated");
-		    	callback({status: "ok", message: "Ok", data: results});
-		    }
+	console.log("Department model connecting to database..");
+	database.connect()
+		.then((db) => {
+			if(!db) throw "Department database connection failed";
+			else {
+				console.log(`Department model connected to database: ${db}`);
+				collection = db.collection('lisd_department');
+			}
+		})
+		.catch(console.error)
+		.finally(() => {
+			// if(database) database.closeConnection();
 		});
-	} catch (e) {
-		console.log("Error: " + e);
-		callback({status: "error", message: "Error: " + e});
-	};
-};
 
-exports.addData = function(data, callback) {
-	console.log("Data:", data);
-	try {
-		// Insert the document
-	    collection.insertOne(data, function(err, result) {
-		    if(err) {
-		    	console.log("Errorr: " + err);
-		    	callback({status: "ok", message: "Ok", data: null});
-		    }
-		    else {
-		    	console.log("Added department " + result.ops[0]._id);
-		    	callback({status: "ok", message: "Ok", data: result.ops[0]._id.toString()});
-		    }
-		});
-	} catch (e) {
-		console.log("Error: " + e);
-		callback(false);
-	};
-};
+	var getList = async function(callback) {
+		var results = {
+			department: {}
+		};
 
-exports.removeData = function(id, callback) {
+		try {
+			var cursor = await collection.find({ "isActive": true }, {"_id": 1, "name": 1}).sort({name:1}).toArray();
+			for(let item of cursor) {
+				results.department[item._id] = item.name;
+			};
 
-	try {
-		// Insert the document
-	    collection.deleteOne({ "_id": ObjectId(id)}, function(err, result) {
-		    if(err) {
-		    	console.log("Error: " + err);
-		    	callback({status: "error", message: "Error: " + e});
-		    }
-		    else {
-		    	console.log("Department " + id + " removed. " + result);
-		    	callback({status: "ok", message: "Ok", data: true});
-		    }
-		});
-	} catch (e) {
-		console.log("Error: " + e);
-		callback(false);
+			callback({status: "ok", message: "Ok", data: results});
+		}
+		catch (e) {
+			callback({status: "error", message: "Error: " + e});
+		}
 	};
-};
+
+	var getAll = async function(callback) {
+		try {
+			var results = await collection.find({}, {"_id": 1, "name": 1, "isActive": 1}).sort({name:1}).toArray();
+			callback({status: "ok", message: "Ok", data: results});
+		}
+		catch (e) {
+			callback({status: "error", message: "Error: " + e});
+		}
+	};
+
+	var getData = async function(id, callback) {
+
+		try {
+			var results = await collection.find({ "_id": new ObjectId(id) }, {"_id": 0, "name": 1, "isActive": 1}).toArray();
+			callback({status: "ok", message: "Ok", data: results});
+		}
+		catch (e) {
+			callback({status: "error", message: "Error: " + e});
+		}
+	};
+
+	var updateData = async function(id, data, callback) {
+
+		try {
+			let results = collection.updateOne({"_id": new ObjectId(id)}, {$set: data});
+			console.log("Department " + id + " updated");
+			callback({status: "ok", message: "Ok", data: results});
+
+		} catch (e) {
+			console.log("Error: " + e);
+			callback({status: "error", message: "Error: " + e});
+		};
+	};
+
+	var addData = async function(data, callback) {
+		console.log("Data:", data);
+		try {
+			let result = await collection.insertOne(data);
+			console.log("Added department " + result.insertedId.toString());
+			callback({status: "ok", message: "Ok", data: result.insertedId.toString()});
+
+		} catch (e) {
+			console.log("Error: " + e);
+			callback(false);
+		};
+	};
+
+	var removeData = async function(id, callback) {
+
+		try {
+			let result = await collection.deleteOne({ "_id": new ObjectId(id)});
+			console.log("Department " + id + " removed. " + result);
+			callback({status: "ok", message: "Ok", data: true});
+
+		} catch (e) {
+			console.log("Error: " + e);
+			callback(false);
+		};
+	};
+
+	return {
+		getList: function(callback) {
+			getList(callback);
+		},
+		getAll: function(callback) {
+			getAll(callback);
+		},
+		getData: function(id, callback) {
+			getData(id, callback);
+		},
+		updateData: function(id, data, callback) {
+			updateData(id, data, callback);
+		},
+		addData: function(data, callback) {
+			addData(data, callback);
+		},
+		removeData: function(id, callback) {
+			removeData(id, callback);
+		}
+	};
+
+})()
